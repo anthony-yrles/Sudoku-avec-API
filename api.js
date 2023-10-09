ee0a3f1137519469b6561c2998f8ceba89c20a3c
Sluchai40 <mabbolos8@gmail.com>
2023-10-09 22:38:00 +0200

    tentative de génération de tableau depuis javascript

--- api.js
+++ api.js
@@ -1,4 +1,7 @@
-let grid = fetch('http://31.33.247.37:3000/api/sudoku')
+document.getElementById("new").onclick = function() {remplissage()};
+
+function remplissage() {
+    let grid = fetch('http://31.33.247.37:3000/api/sudoku')
     .then(response => response.json())
     .then(data =>{
         console.log(data);
@@ -8,3 +11,19 @@ let grid = fetch('http://31.33.247.37:3000/api/sudoku')
         console.log(err);
     })
 
+    let sudokuData = grid.sudoku;
+
+    for (let i = 0; i <= sudokuData.lenght; i++) {
+        for (let j = 0; j <= sudokuData[i].lenght; j++) {
+            const tableBody = document.querySelector("body");
+            const tableCreate = document.createElement("table");
+            const tableRow = document.createElement("tr");
+            const tableCase = document.createElement("td");
+            tableCase.innerText = grid.sudoku[i][j];
+
+            tableBody.appendChild(tableCreate);
+            tableCreate.appendChild(tableRow);
+            tableRow.appendChild(tableCase);
+        }
+    }
+}
