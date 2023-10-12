74a6c4f8fff13158b018c887de4e8e34636a1313
Sluchai40 <mabbolos8@gmail.com>
2023-10-12 22:19:28 +0200

    je sais pas si c est a jour

--- api.js
+++ api.js
@@ -11,19 +11,19 @@ function remplissage() {
         console.log(err);
     })
 
-    let sudokuData = grid.sudoku;
-
-    for (let i = 0; i <= sudokuData.lenght; i++) {
-        for (let j = 0; j <= sudokuData[i].lenght; j++) {
-            const tableBody = document.querySelector("body");
-            const tableCreate = document.createElement("table");
-            const tableRow = document.createElement("tr");
-            const tableCase = document.createElement("td");
-            tableCase.innerText = grid.sudoku[i][j];
-
-            tableBody.appendChild(tableCreate);
-            tableCreate.appendChild(tableRow);
-            tableRow.appendChild(tableCase);
-        }
-    }
 }
+// let sudokuData = grid.sudoku;
+
+// for (let i = 0; i < sudokuData.length; i++) {
+//     for (let j = 0; j < sudokuData[i].length; j++) {
+//         const tableBody = document.querySelector(".total");
+//         const tableCreate = document.createElement("table");
+//         const tableRow = document.createElement("tr");
+//         const tableCase = document.createElement("td");
+//         tableCase.innerText = grid.sudoku[i][j];
+
+//         tableBody.appendChild(tableCreate);
+//         tableCreate.appendChild(tableRow);
+//         tableRow.appendChild(tableCase);
+//     }
+// }
