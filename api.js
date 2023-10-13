3aec2b71a11e12971cbd7cec86f9a8b7cc75a484
Sluchai40 <mabbolos8@gmail.com>
2023-10-13 11:51:01 +0200

    ca avance

--- api.js
+++ api.js
@@ -1,42 +1,44 @@
-document.getElementById("new").onclick = function() {remplissage()};
+document.getElementById("new").onclick = function() {
+    creation();
+};
 
-function remplissage() {
+function creation() {
     let grid = fetch('http://31.33.247.37:3000/api/sudoku')
-    .then(response => response.json())
-    .then(data =>{
-        console.log(data);
-        // return data;
-        let sudokuData = data.sudoku;
-        const tableBody = document.querySelector(".total");
-        tableBody.innerHTML = '';
-        for (let i = 0; i < sudokuData.length; i++) {
-            const tableCreate = document.createElement("table");
-            const tableRow = document.createElement("tr");
-            for (let j = 0; j < sudokuData[i].length; j++) {
-                const tableCase = document.createElement("td");
-                tableCase.innerText = sudokuData[i][j];
-                tableRow.appendChild(tableCase);        
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+
+            if (Array.isArray(data)) {
+                const tableBody = document.querySelector(".grille");
+                tableBody.innerHTML = '';
+				const tableCreate = document.createElement("table");
+				
+                for (let i = 0; i < data.length; i++) {
+                    const tableRow = document.createElement("tr");
+
+                    for (let j = 0; j < data[i].length; j++) {
+                        const tableCase = document.createElement("td");
+						
+						if (data[i][j] != '0') {
+							tableCase.innerText = data[i][j];
+						} else {
+							const input = document.createElement('input');
+							input.type = "text";
+							tableCase.appendChild(input);
+						}
+
+                        tableRow.appendChild(tableCase);
+                    }
+
+					tableCreate.appendChild(tableRow);
+					tableBody.appendChild(tableCreate);    
+                }
+            } else {
+                console.log("Les données ne sont pas dans le format attendu.");
             }
-            tableCreate.appendChild(tableRow);
-            tableBody.appendChild(tableCreate);
-        }
-    })
-    .catch(err => {
-        console.log(err);
-    })
-}
-// let sudokuData = grid.sudoku;
-
-// for (let i = 0; i < sudokuData.length; i++) {
-//     for (let j = 0; j < sudokuData[i].length; j++) {
-//         const tableBody = document.querySelector(".total");
-//         const tableCreate = document.createElement("table");
-//         const tableRow = document.createElement("tr");
-//         const tableCase = document.createElement("td");
-//         tableCase.innerText = grid.sudoku[i][j];
-
-//         tableBody.appendChild(tableCreate);
-//         tableCreate.appendChild(tableRow);
-//         tableRow.appendChild(tableCase);
-//     }
-// }
+        })
+        .catch(err => {
+            console.log(err);
+        });
+    }
+    
